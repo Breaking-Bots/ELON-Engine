1bf81dc25766348398284f0e4b6b3de380221013
Hsifeulbhsifder <zaeemkmohamed@gmail.com>
2015-09-13 18:19:04 -0400

    Tweked log tag

--- src/ELON.cpp
+++ src/ELON.cpp
@@ -128,7 +128,7 @@ public:
 
 U32 main(U32 argc, I8** argv) {
 	if (!HALInitialize()){
-		std::cerr<<"FATAL ERROR: HAL could not be initialized"<<std::endl;
+		std::cerr<<"[ERROR] HAL could not be initialized."<<std::endl;
 		return -1;
 	}
 	HALReport(HALUsageReporting::kResourceType_Language, HALUsageReporting::kLanguage_CPlusPlus);
